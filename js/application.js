a11321d39d8889ae4b683d0df0bb6dedfad81e55
Marco Buschini <marco.buschini@gmail.com>
2014-04-11 16:42:40 +0200

    Added javascript to report on the answers with a popup.

--- js/application.js
+++ js/application.js
@@ -1,5 +1,9 @@
 $.ui.ready(function() {
   $("#question-1-answer").bind("click", function(){
-    alert("I was clicked");
+    alert($("#radio_1_1").prop("checked") + " " + $("#radio_1_2").prop("checked") + " " + $("#radio_1_3").prop("checked"));
+  });
+    
+  $("#question-2-answer").bind("click", function(){
+    alert($("#checkbox_2_1").prop("checked") + " " + $("#checkbox_2_2").prop("checked") + " " + $("#checkbox_2_3").prop("checked"));
   });
 });
