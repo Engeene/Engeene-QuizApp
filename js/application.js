62265f92edddf1e5049751f05ec7172c73459e5d
Marco Buschini <marco.buschini@gmail.com>
2014-04-14 15:30:43 +0200

    Removed the javascript that cleared the form, as forms are better cleared by an HTML reset button.

--- js/application.js
+++ js/application.js
@@ -1,21 +1,17 @@
+/**
+ * This function is invoked when the application UI is started,
+ * to set up events handler on UI elements.
+ */
 $.ui.ready(function() {
+  // Here the status of the first panel is read and shown to the
+  // user via a JavaSritp alert via simple true/false labels.
   $("#question-1-answer").bind("click", function(){
     alert($("#radio_1_1").prop("checked") + " " + $("#radio_1_2").prop("checked") + " " + $("#radio_1_3").prop("checked"));
   });
 
-  $("#question-1-clear").bind("click", function(){
-    $("#radio_1_1").prop("checked", false);
-    $("#radio_1_2").prop("checked", false);
-    $("#radio_1_3").prop("checked", false);
-  });
-
+  // Here the status of the second panel is read and shown to the
+  // user via a JavaSritp alert via simple true/false labels.
   $("#question-2-answer").bind("click", function(){
     alert($("#checkbox_2_1").prop("checked") + " " + $("#checkbox_2_2").prop("checked") + " " + $("#checkbox_2_3").prop("checked"));
-  });
-
-  $("#question-2-clear").bind("click", function(){
-    $("#checkbox_2_1").prop("checked", false);
-    $("#checkbox_2_2").prop("checked", false);
-    $("#checkbox_2_3").prop("checked", false);
   });
 });
